bf8d2019ba976971dcf9dff0436a025435931e86
Ray Zhang <m@raycursive.com>
2023-10-15 16:52:38 -0400

    fourtree: concurrency

--- fourtree.hh
+++ fourtree.hh
@@ -5,6 +5,7 @@
 #include "fixsizedkey.hh"
 #include "kvthread.hh"
 #include "mtcounters.hh"
+#include "nodeversion.hh"
 #include "str.hh"
 #include "string.hh"
 #include <mutex>
@@ -15,26 +16,14 @@ namespace fourtree {
 using lcdf::Str;
 using lcdf::String;
 
-template <size_t KeySize=16> struct tree_params {
+template <size_t KeySize = 16> struct tree_params {
   static constexpr int ikey_size = KeySize;
   static constexpr int fanout = 4;
   using value_type = std::string;
   using threadinfo_type = ::threadinfo;
+  using nodeversion_value_type = uint32_t;
 };
 
-class SpinLock {
-private:
-  std::atomic_flag flag = ATOMIC_FLAG_INIT;
-
-public:
-  void lock() {
-    while (flag.test_and_set(std::memory_order_acquire)) {
-    }
-  }
-  void unlock() { flag.clear(std::memory_order_release); }
-};
-
-using scoped_lock =  std::lock_guard<SpinLock>;
 
 template <typename P> class node;
 template <typename P> class cursor;
@@ -57,65 +46,67 @@ public:
 
   static const char *name() { return "fourtree"; }
 
-  void print(FILE* f) const {
-    print(f, root_);
-  }
+  void print(FILE *f) const { print(f, root_); }
 
 private:
   node_type *root_;
 
-  void print(FILE* f, node_type *node) const {
-    print(f, "", node, false, 0);
-  }
+  void print(FILE *f, node_type *node) const { print(f, "", node, false, 0); }
 
-  void print(FILE* f,const std::string& prefix, const node_type* node, bool isLeft, int layer) const {
-      if (node != nullptr) {
-        fprintf(f, "%s", prefix.c_str());
+  void print(FILE *f, const std::string &prefix, const node_type *node,
+             bool isLeft, int layer) const {
+    if (node != nullptr) {
+      fprintf(f, "%s", prefix.c_str());
 
-        fprintf(f, "%s C(%d)= ", (isLeft ? "├──" : "└──"), layer);
+      fprintf(f, "%s C(%d)= ", (isLeft ? "├──" : "└──"), layer);
 
-        // print the value of the node
-        for (int i=0; i<P::fanout-1; i++) {
-          if (node->values_[i] == nullptr) {
-            fprintf(f, "%d:(%s:%s); ", i, "null", "null");
-          } else {
-            key_type k(node->keys0_[i], node->keys1_[i]);
-            fprintf(f, "%d:(%s:%s); ", i, k.unparse_printable().c_str(), node->values_[i]->data());
-          }
-        }
-        fprintf(f, "\n");
-
-        // print next level
-        for (int i=0; i<P::fanout; i++) {
-          print(f, prefix + (isLeft ? "│   " : "    "), node->children_[i], i!=P::fanout-1, i);
+      // print the value of the node
+      for (int i = 0; i < P::fanout - 1; i++) {
+        if (node->values_[i] == nullptr) {
+          fprintf(f, "%d:(%s:%s); ", i, "null", "null");
+        } else {
+          key_type k(node->keys0_[i], node->keys1_[i]);
+          fprintf(f, "%d:(%s:%s); ", i, k.unparse_printable().c_str(),
+                  node->values_[i]->data());
         }
       }
+      fprintf(f, "\n");
+
+      // print next level
+      for (int i = 0; i < P::fanout; i++) {
+        print(f, prefix + (isLeft ? "│   " : "    "), node->children_[i],
+              i != P::fanout - 1, i);
+      }
     }
+  }
 };
 
-template <typename P> class alignas(CACHE_LINE_SIZE) node {
+template <typename P>
+class alignas(CACHE_LINE_SIZE) node
+    : public nodeversion<nodeversion_parameters<typename P::nodeversion_value_type>> {
   using key_type = fix_sized_key<P::ikey_size>;
   using value_type = typename P::value_type;
   using threadinfo = typename P::threadinfo_type;
   using node_type = node<P>;
+  using nodeversion_type = nodeversion<nodeversion_parameters<typename P::nodeversion_value_type>>;
 
 public:
   // first cacheline contains first 8 bytes of keys and 4 children
-  SpinLock lk_;
-  uint64_t keys0_[P::fanout-1] = {};
-  node<P>* children_[P::fanout] = {};
-  uint64_t keys1_[P::fanout-1] = {};
-  value_type* values_[P::fanout-1] = {};
+  uint64_t keys0_[P::fanout - 1] = {};
+  node<P> *children_[P::fanout] = {};
+  uint64_t keys1_[P::fanout - 1] = {};
+  value_type *values_[P::fanout - 1] = {};
 
-  node(){}
+  node() {}
 
   static node_type *make(threadinfo &ti) {
-    node_type* data = (node_type*) ti.pool_allocate(sizeof(node_type), memtag_masstree_internode);
+    node_type *data = (node_type *)ti.pool_allocate(sizeof(node_type),
+                                                    memtag_masstree_internode);
     new (data) node_type();
     return data;
   }
 
-  int compare_with(const key_type& k, size_t index) const {
+  int compare_with(const key_type &k, size_t index) const {
     int cmp = ::compare(k.ikey_u.ikey[0], keys0_[index]);
     if (cmp == 0) {
       cmp = ::compare(k.ikey_u.ikey[1], keys1_[index]);
@@ -123,14 +114,54 @@ public:
     return cmp;
   }
 
-  void assign(const key_type& k, value_type v, size_t index, threadinfo& ti) {
-    value_type* pNewValue = (value_type*) ti.pool_allocate(sizeof(value_type), memtag_value);
+  void assign(const key_type &k, value_type v, size_t index, threadinfo &ti) {
+    value_type *pNewValue =
+        (value_type *)ti.pool_allocate(sizeof(value_type), memtag_value);
     new (pNewValue) value_type(v);
-    lk_.lock();
     keys0_[index] = k.ikey_u.ikey[0];
     keys1_[index] = k.ikey_u.ikey[1];
+    value_type *oldValue = values_[index];
     values_[index] = pNewValue;
-    lk_.unlock();
+    if (oldValue != nullptr) {
+      ti.pool_deallocate(oldValue, sizeof(value_type), memtag_value);
+    }
+  }
+
+  node_type *route(const key_type &k, size_t &index) const {
+    retry:
+    auto v = this->stable();
+    for (int i = 0; i < P::fanout - 1; i++) {
+      // no value in current index
+      if (values_[i] == nullptr) {
+        if (unlikely(this->has_changed(v))) {
+          goto retry;
+        }
+        index = i;
+        return const_cast<node_type *>(this);
+      }
+      int cmp = compare_with(k, i);
+      if (cmp == 0) {
+        // found
+        if (unlikely(this->has_changed(v))) {
+          goto retry;
+        }
+        index = i;
+        return const_cast<node_type *>(this);
+      }
+      if (cmp < 0) {
+        if (unlikely(this->has_changed(v))) {
+          goto retry;
+        }
+        index = i;
+        return children_[i];
+      }
+      // proceed to next index
+    }
+    if (unlikely(this->has_changed(v))) {
+      goto retry;
+    }
+    index = P::fanout - 1;
+    return children_[P::fanout - 1];
   }
 };
 
@@ -146,47 +177,50 @@ public:
   using threadinfo = typename P::threadinfo_type;
 
   cursor(const four_tree<P> &tree, Str key)
-      : parent_(nullptr), node_(nullptr), index_(0), k_(key), pv_(nullptr), root_(tree.root()) {}
+      : parent_(nullptr), node_(nullptr), index_(0), k_(key), pv_(nullptr),
+        root_(tree.root()) {}
 
   cursor(const four_tree<P> &tree, const char *key)
-      : parent_(nullptr), node_(nullptr), index_(0), k_(key_type(key)), pv_(nullptr), root_(tree.root()) {}
+      : parent_(nullptr), node_(nullptr), index_(0), k_(key_type(key)),
+        pv_(nullptr), root_(tree.root()) {}
 
-  bool find() {
+  void find_unlocked() {
     node_ = const_cast<node_type *>(root_);
+    index_ = 0;
 
+    while (node_) {
+      node_type *next = node_->route(k_, index_);
+      if (next == node_) {
+        pv_ = node_->values_[index_];
+        return;
+      } else {
+        parent_ = node_;
+        node_ = next;
+      }
+    }
+    // reach an empty node
+  }
+
+  void find_locked() {
+    node_ = const_cast<node_type *>(root_);
     forward:
     while (node_) {
-      node_->lk_.lock();
-      for (int i=0; i<P::fanout-1; i++) {
-        // value not exists
-        if (node_->values_[i] == nullptr) {
-          index_ = i;
-          pv_ = node_->values_[index_];
-          node_->lk_.unlock();
-          return false;
-        }
-        int cmp = node_->compare_with(k_, i);
-        if (cmp == 0) {
-          index_ = i;
-          pv_ = node_->values_[i];
-          node_->lk_.unlock();
-          return true;
-        } else if (cmp < 0) {
-          parent_ = node_;
-          node_ = node_->children_[i];
-          index_ = i;
-          parent_->lk_.unlock();
-          goto forward;
-        }
+      node_->lock();
+      if (parent_) {
+        parent_->unlock();
       }
-      parent_ = node_;
-      index_ = P::fanout-1;
-      node_ = node_->children_[P::fanout-1];
-      parent_->lk_.unlock();
+      index_ = 0;
+      node_type *next = node_->route(k_, index_);
+      if (next == node_) {
+        // found the existed node
+        pv_ = node_->values_[index_];
+        return;
+      } else {
+        parent_ = node_;
+        node_ = next;
+      }
     }
-    // node_ = null, i.e. new node
-    // keep the last index
-    return false;
+    // reach an empty node
   }
 
 private:
@@ -204,43 +238,32 @@ class query {
 public:
   template <typename T> typename T::value_type *get(T &tree, Str key) {
     typename T::cursor_type c(tree, key);
-    if (c.find()) {
-      return c.pv_;
-    } else {
-      return nullptr;
-    }
+    c.find_unlocked();
+    return c.pv_;
   }
 
   template <typename T>
   void put(T &tree, Str key, typename T::value_type value, threadinfo &ti) {
-    using value_type = typename T::value_type;
     using node_type = typename T::node_type;
+    using key_type = typename T::key_type;
 
     typename T::cursor_type c(tree, key);
-    bool found = c.find();
-    if (c.node_ == nullptr) {
+    c.find_locked();
+    if (!c.node_) {
+      c.parent_->mark_insert();
       // create new node and save to the first
-      node_type* node = node_type::make(ti);
-      node->assign(key, value, 0, ti);
-      c.parent_->lk_.lock();
+      node_type *node = node_type::make(ti);
+      node->assign(key_type(key), value, 0, ti);
       c.parent_->children_[c.index_] = node;
-      c.parent_->lk_.unlock();
+      c.parent_->unlock();
     } else {
-      if (found) {
-        // update the value
-        c.node_->lk_.lock();
-        *c.node_->values_[c.index_] = value;
-        c.node_->lk_.unlock();
-      } else {
-        c.node_->assign(key, value, c.index_, ti);
-      }
+      c.node_->mark_insert();
+      c.node_->assign(key_type(key), value, c.index_, ti);
+      c.node_->unlock();
     }
-
   }
 };
 
 using default_table = four_tree<tree_params<16>>;
-// static_assert(sizeof(default_table::node_type) == 40,
-//               "binary tree node size is not 40 bytes");
-} // namespace binarytree
+} // namespace fourtree
 #endif
